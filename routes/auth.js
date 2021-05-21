d68a86cdeb2658dffae4aefdb61b47821866f301
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-05-21 15:08:19 -0400

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,37 @@
  *
  **/
 
+// Using JWTs in Express
+// Login
+// demo/auth-api/routes/auth.js
+// /** (Fixed) Login: returns JWT on success. */
+
+// router.post("/login", async function (req, res, next) {
+//   try {
+//     const { username, password } = req.body;
+//     const result = await db.query(
+//       "SELECT password FROM users WHERE username = $1",
+//       [username]);
+//     let user = result.rows[0];
+
+//     if (user) {
+//       if (await bcrypt.compare(password, user.password) === true) {
+//         let token = jwt.sign({ username }, SECRET_KEY);
+//         return res.json({ token });
+//       }
+//     }
+//     throw new ExpressError("Invalid user/password", 400);
+//   } catch (err) {
+//     return next(err);
+//   }
+// });
+
+
+
+
+
+
+
 
 /** POST /register - register user: registers, logs in, and returns token.
  *
