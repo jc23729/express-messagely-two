270f0a0acf0045e4f7d717693d41dd35496b6d37
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-05-21 15:09:59 -0400

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,5 @@
+
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
@@ -6,28 +8,30 @@
 
 // Using JWTs in Express
 // Login
-// demo/auth-api/routes/auth.js
+// demo/auth-api/routes/auth.jS
+
+
 // /** (Fixed) Login: returns JWT on success. */
 
-// router.post("/login", async function (req, res, next) {
-//   try {
-//     const { username, password } = req.body;
-//     const result = await db.query(
-//       "SELECT password FROM users WHERE username = $1",
-//       [username]);
-//     let user = result.rows[0];
+router.post("/login", async function (req, res, next) {
+  try {
+    const { username, password } = req.body;
+    const result = await db.query(
+      "SELECT password FROM users WHERE username = $1",
+      [username]);
+    let user = result.rows[0];
 
-//     if (user) {
-//       if (await bcrypt.compare(password, user.password) === true) {
-//         let token = jwt.sign({ username }, SECRET_KEY);
-//         return res.json({ token });
-//       }
-//     }
-//     throw new ExpressError("Invalid user/password", 400);
-//   } catch (err) {
-//     return next(err);
-//   }
-// });
+    if (user) {
+      if (await bcrypt.compare(password, user.password) === true) {
+        let token = jwt.sign({ username }, SECRET_KEY);
+        return res.json({ token });
+      }
+    }
+    throw new ExpressError("Invalid user/password", 400);
+  } catch (err) {
+    return next(err);
+  }
+});
 
 
 
