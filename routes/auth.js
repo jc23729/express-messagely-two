7f20996e954cdd01aa2cc6179832e2c8919d12b4
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-05-21 16:19:28 -0400

    update class user in user.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,4 @@
 
-
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
@@ -13,25 +12,25 @@
 
 // /** (Fixed) Login: returns JWT on success. */
 
-// router.post("/login", async function (req, res, next) {
-//   try {
-//     const { username, password } = req.body;
-//     const result = await db.query(
-//       "SELECT password FROM users WHERE username = $1",
-//       [username]);
-//     let user = result.rows[0];
+router.post("/login", async function (req, res, next) {
+  try {
+    const { username, password } = req.body;
+    const result = await db.query(
+      "SELECT password FROM users WHERE username = $1",
+      [username]);
+    let user = result.rows[0];
 
-//     if (user) {
-//       if (await bcrypt.compare(password, user.password) === true) {
-//         let token = jwt.sign({ username }, SECRET_KEY);
-//         return res.json({ token });
-//       }
-//     }
-//     throw new ExpressError("Invalid user/password", 400);
-//   } catch (err) {
-//     return next(err);
-//   }
-// });
+    if (user) {
+      if (await bcrypt.compare(password, user.password) === true) {
+        let token = jwt.sign({ username }, SECRET_KEY);
+        return res.json({ token });
+      }
+    }
+    throw new ExpressError("Invalid user/password", 400);
+  } catch (err) {
+    return next(err);
+  }
+});
 
 
 
